8ea39546239816fb1ca1d91a2e056c89266dbfe4
Joel Davenport <joel.davenport@ntient.ai>
2022-01-27 15:43:56 -0500

    Add Packager

---
+++ tests/test_packager.py
@@ -0,0 +1,40 @@
+import pytest
+import joblib
+from sklearn.tree import DecisionTreeClassifier
+from tensorflow import keras
+import torch
+import os
+
+def load_sklearn():
+    return joblib.load("tests/sklearn_model.joblib")
+
+def load_keras():
+    return keras.models.load_model("tests/keras_model.h5")
+
+def load_pytorch():
+    return torch.load("tests/pytorch_model.pt")
+
+
+def test_packager_packages_sklearn(cli_context):
+    import ntient
+    packager = ntient.Packager(load_sklearn())
+
+    fn = packager.package_sklearn_model()
+
+    os.remove(fn)
+
+def test_packager_packages_keras(cli_context):
+    import ntient
+    packager = ntient.Packager(load_keras())
+
+    fn = packager.package_keras_model()
+
+    os.remove(fn)
+
+def test_packager_packages_pytorch(cli_context):
+    import ntient
+    packager = ntient.Packager(load_pytorch())
+
+    fn = packager.package_pytorch_model()
+
+    os.remove(fn)
